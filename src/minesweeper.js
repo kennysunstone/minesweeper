1b7a506aef07ee8c9fdcd7388c1650ee349e5281
kennysunstone <kenny@sunstonecommunication.com>
2017-11-26 17:31:27 +0000

    Complete step 3 of project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,42 @@
-const exampleBoard = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-]
-const printBoard = board => {
-  console.log('Current Board');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-printBoard (exampleBoard);
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board =[];
+  for(let rowIndex = 0;rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for(let columnIndex=0; columnIndex < numberOfColumns;columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board =[];
+  for(let rowIndex = 0;rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for(let columnIndex=0; columnIndex < numberOfColumns;columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  //Loop is fine but can placed 2 bombs in one spcae - fix with control flow
+  while (numberOfBombsPlaced<numberOfBombs) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board [randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+  return board;
+}
+const printBoard =(board) => {
+  console.log(board.map(row => row.join('|')).join('\n'));
+}
 
-exampleBoard[0][1]='1';
-exampleBoard[2][2]='B';
-printBoard (exampleBoard);
+const playerBoard = (generatePlayerBoard(9,9));
+const bombBoard = (generateBombBoard(9,9,24));
+
+console.log('Player board:');
+printBoard(playerBoard)
+
+console.log('Bomb board:');
+printBoard(bombBoard);
