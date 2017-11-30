be8d2f2fd58a49923baf73c739760b26ff2ff3f6
kennysunstone <kenny@sunstonecommunication.com>
2017-11-30 16:58:29 +0000

    Complete step 4

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,4 @@
+//This is the blank board to accept guesses
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board =[];
   for(let rowIndex = 0;rowIndex < numberOfRows; rowIndex++) {
@@ -9,6 +10,7 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board;
 }
+//This will be hidden and show where the bombs are
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   const board =[];
   for(let rowIndex = 0;rowIndex < numberOfRows; rowIndex++) {
@@ -18,25 +20,66 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     }
     board.push(row);
   }
+
   let numberOfBombsPlaced = 0;
-  //Loop is fine but can placed 2 bombs in one spcae - fix with control flow
+  //Loop is fine with if/else and board array reference to avoid two bombs in one space
   while (numberOfBombsPlaced<numberOfBombs) {
     const randomRowIndex = Math.floor(Math.random() * numberOfRows);
     const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    if(board[randomRowIndex][randomColumnIndex] !== 'B'){
     board [randomRowIndex][randomColumnIndex] = 'B';
     numberOfBombsPlaced++;
   }
+  }
   return board;
 }
-const printBoard =(board) => {
+
+//This piece calculates the number of adjacent bombs - complex
+const getNumberOfNeighbourBombs = (bombBoard,rowIndex,columnIndex) => {
+  const neighbourOffsets = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs =0;
+  neighbourOffsets.forEach(offset => {
+    const neighbourRowIndex = rowIndex + offset[0];
+    const neighbourColumnIndex = columnIndex + offset[1];
+    if(neighbourRowIndex >= 0 && neighbourRowIndex < numberOfRows &&
+      neighbourColumnIndex >= 0 && neighbourColumnIndex < numberOfColumns) {
+        if(bombBoard[neighbourRowIndex][neighbourColumnIndex] === 'B') {
+          numberOfBombs++;
+        }
+      }
+    });
+      return numberOfBombs;
+  }
+  //Now to add the fliptile option
+  const flipTile = (playerBoard,bombBoard,rowIndex,columnIndex) => {
+    if(playerBoard[rowIndex][columnIndex] !== ' ') {
+      console.log('This tile has already been flipped');
+      return;
+    }
+
+    if(bombBoard[rowIndex][columnIndex] === 'B') {
+      playerBoard[rowIndex][columnIndex] = 'B';
+    } else {
+      playerBoard[rowIndex][columnIndex] =
+      getNumberOfNeighbourBombs(bombBoard,rowIndex,columnIndex);
+    }
+  }
+
+const printBoard = (board) => {
   console.log(board.map(row => row.join('|')).join('\n'));
 }
 
-const playerBoard = (generatePlayerBoard(9,9));
-const bombBoard = (generateBombBoard(9,9,24));
+const playerBoard = (generatePlayerBoard(3,3));
+const bombBoard = (generateBombBoard(3,3,4));
 
 console.log('Player board:');
-printBoard(playerBoard)
+printBoard(playerBoard);
 
 console.log('Bomb board:');
 printBoard(bombBoard);
+
+flipTile(playerBoard,bombBoard,0,0);
+console.log('Updated Player Board:');
+printBoard(playerBoard);
